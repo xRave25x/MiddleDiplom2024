4e1b1b412c9adde5ff0ed3606657d2c0c3b32e5f
Andrei Demichev <andyuson24@gmail.com>
2024-02-09 13:29:24 +0200

    Add second modal window

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,20 +1,27 @@
 const modal = () => {
     const overlay = document.querySelector('.overlay');
     const headerModal = document.querySelector('.header-modal');
-    const buttonCall = document.querySelector('.button');
+    const btnCall = document.querySelector('.button');
+    const servicesBtns = document.querySelectorAll('.service-button');
+    const servicesModal = document.querySelector('.services-modal');
 
-    buttonCall.addEventListener('click', () => {
+    btnCall.addEventListener('click', () => {
         overlay.style.display = 'block';
         headerModal.style.display = 'block';
     })
 
-    headerModal.addEventListener('click', e => {
-        if (e.target.classList.contains('header-modal__close')) {
+    servicesBtns.forEach(button => button.addEventListener('click', () => {
+        servicesModal.style.display = 'block';
+        overlay.style.display = 'block';
+    }))
+
+    document.addEventListener('click', e => {
+        if (e.target.classList.contains('services-modal__close') || e.target.classList.contains('header-modal__close') || e.target.classList.contains('overlay')) {
             headerModal.style.display = 'none';
+            servicesModal.style.display = 'none';
             overlay.style.display = 'none';
         }
     });
-
 
 }
 
